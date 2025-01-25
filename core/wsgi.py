6de2cd9a53dee92d6e022fc7c9f0a6f3b69f1370
zainchodry <razzaqzain981@gmail.com>
2025-01-25 12:48:50 -0800

    change

--- core/wsgi.py
+++ core/wsgi.py
@@ -7,14 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+# core/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-app = application
 
 
